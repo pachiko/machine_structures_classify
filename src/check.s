1bc76c5010596617d79633f75bc56720f0cc16e2
pachiko <phillippang1994@gmail.com>
2023-02-09 23:20:51 +0800

    task 7: read matrix

--- src/check.s
+++ src/check.s
@@ -1,4 +1,4 @@
-.globl check_size, check_stride, check_mat_dims, check_matmul_rc
+.globl check_size, check_stride, check_mat_dims, check_matmul_rc, check_malloc, check_file_open, check_file_close, check_file_read
 
 .text
 check_size:
@@ -18,15 +18,38 @@ check_mat_dims: # a0, a1 >= 1
     bge a0, t0, check_width
     li a0 38
     j exit
+    
 check_width: 
     bge a1, t0, passed_check
     li a0 38
     j exit
     
- check_matmul_rc: # a0 == a1
+check_matmul_rc: # a0 == a1
     beq a0, a1, passed_check
     li a0 38
     j exit
 
+check_malloc:
+    bne a0, x0, passed_check
+    li a0 26
+    j exit
+
+check_file_open:
+    addi t0, x0, -1
+    bne a0, t0, passed_check
+    li a0 27
+    j exit
+    
+check_file_close:
+    addi t0, x0, -1
+    bne a0, t0, passed_check
+    li a0 28
+    j exit
+    
+check_file_read: # a0 == a1
+    beq a0, a1, passed_check
+    li a0 29
+    j exit
+    
 passed_check:
     jr ra
--- src/read_matrix.s
+++ src/read_matrix.s
@@ -1,3 +1,4 @@
+.import check.s
 .globl read_matrix
 
 .text
@@ -25,18 +26,88 @@
 #     this function terminates the program with error code 29
 # ==============================================================================
 read_matrix:
+    # PROLOGUE
+    addi sp, sp, -28
+    sw s0, 0(sp) # row*
+    sw s1, 4(sp) # col*
+    sw s2, 8(sp) # file*
+    sw s3, 12(sp) # array*
+    sw s4, 16(sp) # bytes in array
+    sw ra, 20(sp)
+    
+    # STORE RETURN PTR
+    add s0, x0, a1 # return row*
+    add s1, x0, a2 # return col*
+    
+    # READ & CHECK FILE
+    add a1, x0, x0 # read permission
+    jal fopen
+    add s2, x0, a0 # file descriptor
+    jal check_file_open # check if file descriptor == -1
 
-    # Prologue
-
-
-
-
-
-
-
-
-
-    # Epilogue
-
-
+    # ALLOCATE BUFFER FOR # ROW, # COL
+    addi a0, x0, 8 # 2 x 4 bytes
+    jal malloc # allocate memory for fread
+    jal check_malloc # check if a0 == 0
+    
+    # READ # ROW, # COL
+    add a1, a0, x0 # buffer
+    add a0, x0, s2 # file descriptor
+    addi a2, x0, 8 # 2 x 4 bytes
+    addi sp, sp, -4
+    sw a1, 0(sp)
+    jal fread
+    
+    # CHECK READ
+    addi a1, x0, 8 # byte check
+    jal check_file_read
+    lw a1, 0(sp)
+    addi sp, sp 4
+    
+    # WRITE # ROW # COL, FREE BUFFER
+    lw t0, 0(a1) # numRows
+    lw t1, 4(a1) # numCols
+    sw t0, 0(s0)
+    sw t1, 0(s1)
+    
+    add a0, a1, x0 # move buffer ptr
+    jal free # free the buffer
+    
+    lw t0, 0(s0)
+    lw t1, 0(s1)
+    
+    # ALLOCATE ARRAY
+    mul s4, t0, t1 # numElems
+    slli s4, s4, 2 # in bytes...
+    add a0, x0, s4
+    jal malloc # allocate memory for fread
+    jal check_malloc # check if a0 == 0
+    add s3, a0, x0 # store the array ptr
+   
+    # SETUP READ ARGS, READ MATRIX
+    add a0, x0, s2
+    add a1, x0, s3
+    add a2, x0, s4
+    jal fread # read the matrix
+    add a1, x0, s4
+    jal check_file_read
+         
+    # CLOSE FILE
+    add a0, x0, s2
+    jal fclose
+    jal check_file_close
+    
+    add a0, x0, s3
+    
+    # EPILOGUE
+    lw s0, 0(sp)
+    lw s1, 4(sp)
+    lw s2, 8(sp)
+    lw s3, 12(sp)
+    lw s4, 16(sp)
+    lw ra, 20(sp)
+    addi sp, sp, 28
+    
     jr ra
+    
+    
